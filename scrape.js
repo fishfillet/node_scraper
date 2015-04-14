7284b63d85591f27219b604d06ae16413652e0e7
fishfillet <daniel.susetyo@gmail.com>
2015-04-14 20:25:59 +0000

    regex filetype

--- scrape.js
+++ scrape.js
@@ -1,22 +1,38 @@
 var cheerio = require('cheerio');
 var request = require('request');
+var fs = require('fs');
 
 
 request('http://substack.net/images/', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body) 
+    // console.log(body) 
     parseBody(body);
   }
 })
 
 function parseBody(data){
-  console.log('data loaded');
+  var typeCheck = /(\.|\/)(gif|jpe?g|png|txt|svg)$/i
+  // console.log('data loaded');
+
   $ = cheerio.load(data);
-  console.log($('tr').contents().length);
+  // console.log($('tr').contents().length);
+
   $('tr').each(function(index, element){
-    console.log($(this).children().first().text());
-    console.log($(this).find('a').attr('href'));
-    console.log($(this).children().last().text());
+    var permission = $(this).children().first().text();
+    var absoluteUrl = $(this).find('a').attr('href');
+    var images = $(this).children().last().text();
+    var check = typeCheck.exec(images);
+
+
+    if (check){
+      var fileType = check[0];
+      console.log(fileType);
+    }
+
+
+
+
   });
 
 }
+
